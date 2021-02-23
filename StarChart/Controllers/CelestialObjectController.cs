4ac4130471520e9c1183cebb8d7e368d75ac1a97
Milad Vafaeifard <milad.vafaeifard@gmail>
2021-02-23 22:32:43 +0100

    Create GetById Action

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -16,6 +17,32 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.SingleOrDefault(cb => cb.Id == id);
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+
+            celestialObject.Satellites = new List<CelestialObject>()
+            {
+                new CelestialObject()
+                {
+                    Id = 1,
+                    Name = "Earth"
+                },
+                new CelestialObject()
+                {
+                    Id = 2,
+                    Name = "Moon",
+                    OrbitedObjectId = 1
+                }
+            };
+            return Ok(celestialObject);
         }
 
     }
